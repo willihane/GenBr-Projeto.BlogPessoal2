942d3dea21165fed5f5a6e1235d4812ce7705320
willihane <williane.pereira@outlook.com>
2022-06-21 17:34:09 -0300

    Implementação de barra de pesquisas por titulo e tema

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -18,10 +18,12 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
   idTema: number
+  nomeTema: string
 
   usuario: Usuario = new Usuario()
   idUsuario = environment.id
@@ -90,5 +92,26 @@ export class InicioComponent implements OnInit {
     this.getAllPostagens()
     })
     }
+
+    
+    findByTituloPostagem(){
+      if(this.tituloPost == ''){
+        this.getAllPostagens()
+        } else {
+      this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[])=>{
+        this.listaPostagens = resp
+      })
+    }
+  }
+  
+  findByDescricaoTema() {
+    if(this.nomeTema == ''){
+      this.getAllTemas()
+      } else {
+      this.temaService.getByDescricaoTema(this.nomeTema).subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+      })
+    }
+  }
 
 }
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -14,29 +14,30 @@ export class TemaService {
   token = {
     headers: new HttpHeaders().set('Authorization',environment.token)
   }
-// **Métodos Http**
 
-//Método GET: Aqui busca todos os temas:
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://willihane.herokuapp.com/temas', this.token)
   }
 
-              //Aqui busca por id do tema:
+
   getByIdTema(id: number): Observable<Tema>{
     return this.http.get<Tema>(`https://willihane.herokuapp.com/temas/${id}`, this.token)
   }
 
-//Método POST: Aqui insere novos temas:
+  getByDescricaoTema(descricao: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://willihane.herokuapp.com/temas/descricao/${descricao}`, this.token)
+  }
+
 postTema(tema: Tema): Observable<Tema>{
   return this.http.post<Tema>('https://willihane.herokuapp.com/temas', tema,this.token)
 }
 
-//Método PUT: Aqui atualiza por tema:
+
 putTema(tema: Tema): Observable<Tema>{
   return this.http.put<Tema>('https://willihane.herokuapp.com/temas', tema, this.token)
 }
 
-//Método DELETE: Aqui exclui:
+
 deleteTema(id: number): Observable<Object> {
   return this.http.delete(`https://willihane.herokuapp.com/temas/${id}`, this.token)
 }
